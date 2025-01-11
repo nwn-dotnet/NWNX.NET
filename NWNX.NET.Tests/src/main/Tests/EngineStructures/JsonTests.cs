c6265b149cb18fe291df724f04bba12dc0f9cabe
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2025-01-11 02:00:28 +0100

    Json struct tests.

---
+++ NWNX.NET.Tests/src/main/Tests/EngineStructures/JsonTests.cs
@@ -0,0 +1,34 @@
+using System;
+using NUnit.Framework;
+using NWNX.NET.Tests.Constants;
+using NWNX.NET.Tests.Native;
+
+namespace NWNX.NET.Tests.EngineStructures
+{
+  [TestFixture(Category = "Engine Structures")]
+  public class JsonTests
+  {
+    [Test]
+    public void JsonTest()
+    {
+      const string jsonString = "[1,2,3]";
+
+      NWNXAPI.StackPushString(jsonString);
+      NWNXAPI.CallBuiltIn(VMFunctions.JsonParse);
+      using EngineStructure jsonArray = new EngineStructure(EngineStructureType.Json, NWNXAPI.StackPopGameDefinedStructure(EngineStructureType.Json));
+
+      Assert.That(jsonArray.Pointer, Is.Not.EqualTo(IntPtr.Zero));
+
+      NWNXAPI.StackPushInteger(1);
+      NWNXAPI.StackPushGameDefinedStructure(EngineStructureType.Json, jsonArray);
+      NWNXAPI.CallBuiltIn(VMFunctions.JsonArrayGet);
+      using EngineStructure jsonArrayValue2 = new EngineStructure(EngineStructureType.Json, NWNXAPI.StackPopGameDefinedStructure(EngineStructureType.Json));
+
+      NWNXAPI.StackPushGameDefinedStructure(EngineStructureType.Json, jsonArrayValue2);
+      NWNXAPI.CallBuiltIn(VMFunctions.JsonGetInt);
+      int value = NWNXAPI.StackPopInteger();
+
+      Assert.That(value, Is.EqualTo(2));
+    }
+  }
+}
