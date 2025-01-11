656f588599b1b1528dc5f9a7c3c629524ea78141
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2025-01-11 01:59:29 +0100

    Add additional test functions.

--- NWNX.NET.Tests/src/main/Constants/VMFunctions.cs
+++ NWNX.NET.Tests/src/main/Constants/VMFunctions.cs
@@ -2,6 +2,8 @@ namespace NWNX.NET.Tests.Constants
 {
   public static class VMFunctions
   {
+    public const int PrintString = 1;
+    public const int ExecuteScript = 8;
     public const int SubString = 65;
     public const int fabs = 67;
     public const int abs = 77;
@@ -13,6 +15,7 @@ namespace NWNX.NET.Tests.Constants
     public const int GetPositionFromLocation = 223;
     public const int GetAreaFromLocation = 224;
     public const int GetFacingFromLocation = 225;
+    public const int GetModule = 242;
     public const int GetUserDefinedEventNumber = 247;
     public const int GetName = 253;
     public const int TalentSpell = 301;
@@ -21,11 +24,20 @@ namespace NWNX.NET.Tests.Constants
     public const int GetIsTalentValid = 359;
     public const int GetTypeFromTalent = 362;
     public const int EffectNegativeLevel = 462;
+    public const int ItemPropertyHaste = 647;
     public const int GetEffectTag = 849;
     public const int TagEffect = 850;
     public const int GetModuleName = 561;
+    public const int GetItemPropertyTag = 854;
+    public const int TagItemProperty = 855;
     public const int GetFirstArea = 861;
     public const int SetEventScript = 886;
     public const int GetEffectInteger = 939;
+    public const int GetLocalCassowary = 945;
+    public const int CassowaryConstrain = 949;
+    public const int CassowaryGetValue = 951;
+    public const int JsonParse = 968;
+    public const int JsonGetInt = 981;
+    public const int JsonArrayGet = 987;
   }
 }
