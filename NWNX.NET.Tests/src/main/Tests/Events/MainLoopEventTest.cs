7642abf0aab0eecc229b03acbc5e89148f157493
Jhett Black <10942655+jhett12321@users.noreply.github.com>
2025-01-11 02:00:52 +0100

    Main loop event tests.

---
+++ NWNX.NET.Tests/src/main/Tests/Events/MainLoopEventTest.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NWNX.NET.Tests.Async;
+
+namespace NWNX.NET.Tests.Events
+{
+  [TestFixture(Category = "Events")]
+  public class MainLoopEventTest
+  {
+    private const int FrameCount = 60;
+
+    private bool testRunning;
+
+    private readonly List<ulong> loopFrames = [];
+    private readonly List<int> loopThreads = [];
+
+    [SetUp]
+    public void Setup()
+    {
+      Program.OnMainLoop += OnMainLoop;
+    }
+
+    [Test]
+    [Timeout(5000)]
+    public async Task MainLoopTest()
+    {
+      int mainThreadId = Thread.CurrentThread.ManagedThreadId;
+      testRunning = true;
+
+      CancellationTokenSource cts = new CancellationTokenSource();
+      cts.CancelAfter(TimeSpan.FromSeconds(5));
+
+      await NwTask.WaitUntil(() => !testRunning, cts.Token);
+
+      Assert.That(loopFrames.Count, Is.EqualTo(FrameCount));
+      Assert.That(loopThreads.Count, Is.EqualTo(FrameCount));
+
+      ulong[] expectedLoopFrames = Enumerable.Range((int)loopFrames[0], FrameCount).Select(i => (ulong)i).ToArray();
+      int[] expectedLoopThreads = Enumerable.Repeat(mainThreadId, FrameCount).ToArray();
+
+      Assert.That(loopFrames, Is.EqualTo(expectedLoopFrames));
+      Assert.That(loopThreads, Is.EqualTo(expectedLoopThreads));
+    }
+
+    private void OnMainLoop(ulong frame)
+    {
+      if (!testRunning)
+      {
+        return;
+      }
+
+      if (loopFrames.Count < FrameCount)
+      {
+        loopFrames.Add(frame);
+        loopThreads.Add(Thread.CurrentThread.ManagedThreadId);
+      }
+      else
+      {
+        testRunning = false;
+      }
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+      Program.OnMainLoop -= OnMainLoop;
+      loopFrames.Clear();
+      loopThreads.Clear();
+    }
+  }
+}
